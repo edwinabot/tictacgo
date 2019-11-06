902d3e9100d04dace50a22d9f3e3f4292883cb6a
Edwin <edwinabot@gmail.com>
2019-11-06 16:16:09 -0300

    not working... yet

--- learn.go
+++ learn.go
@@ -10,9 +10,26 @@ func main() {
 	var turn = -1
 	for !isTicTacToe(grid) {
 		turn *= -1
-		printGrid(grid)
-		var pos = scanMarkPosition()
-		placeMark(grid, pos, turn)
+		if turn == 1 {
+			printGrid(grid)
+			var pos = scanMarkPosition()
+			placeMark(grid, pos, turn)
+		} else {
+			var bestMove int = -1
+			var bestMoveScore int
+			for i := 0; i < 9; i++ {
+				if grid[i] != 0 {
+					grid[i] = -1
+					score := minimax(grid, 6, false)
+					grid[i] = 0
+					if bestMove == -1 || bestMoveScore > score {
+						bestMove = i
+						bestMoveScore = score
+					}
+				}
+			}
+			grid[bestMove] = -1
+		}
 	}
 	fmt.Printf("TicTacGo!! winner is: %v\n", toMarkString(turn))
 }
@@ -36,6 +53,18 @@ func isTicTacToe(grid []int) bool {
 		}
 	}
 	return false
+}
+
+func isDraw(grid []int) bool {
+	if isTicTacToe(grid) {
+		return false
+	}
+	for i := 0; i < 9; i++ {
+		if grid[i] == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func placeMark(grid []int, position int, mark int) {
@@ -62,10 +91,74 @@ func toMarkString(i int) string {
 func printGrid(grid []int) {
 	var st string = "%v|%v|%v\n"
 	var dv string = "-----"
-
 	fmt.Printf(st, toMarkString(grid[6]), toMarkString(grid[7]), toMarkString(grid[8]))
 	fmt.Println(dv)
 	fmt.Printf(st, toMarkString(grid[3]), toMarkString(grid[4]), toMarkString(grid[5]))
 	fmt.Println(dv)
 	fmt.Printf(st, toMarkString(grid[0]), toMarkString(grid[1]), toMarkString(grid[2]))
+}
+
+func evaluate(grid []int, depth int, isMaximizing bool) int {
+	tttValue := 50
+	score := 0
+	if isTicTacToe(grid) {
+		if isMaximizing {
+			score += tttValue
+		} else {
+			score -= tttValue
+		}
+	} else if isDraw(grid) {
+		score = 0
+	}
+	if isMaximizing {
+		score -= depth
+	} else {
+		score += depth
+	}
+	return score
+}
+
+func minimize(grid []int, depth int) int {
+	if depth == 0 || isTicTacToe(grid) || isDraw(grid) {
+		return evaluate(grid, depth, false)
+	}
+
+	bestScore := 1000000
+	for i := 0; i < 9; i++ {
+		if grid[i] == 0 {
+			grid[i] = -1
+			score := maximize(grid, depth-1)
+			grid[i] = 0
+			if score < bestScore {
+				bestScore = score
+			}
+		}
+	}
+	return bestScore
+}
+
+func maximize(grid []int, depth int) int {
+	if depth == 0 || isTicTacToe(grid) || isDraw(grid) {
+		return evaluate(grid, depth, true)
+	}
+
+	bestScore := -1000000
+	for i := 0; i < 9; i++ {
+		if grid[i] == 0 {
+			grid[i] = 1
+			score := minimize(grid, depth-1)
+			grid[i] = 0
+			if score > bestScore {
+				bestScore = score
+			}
+		}
+	}
+	return bestScore
+}
+
+func minimax(grid []int, maxdepth int, isMaximizing bool) int {
+	if isMaximizing {
+		return maximize(grid, maxdepth)
+	}
+	return minimize(grid, maxdepth)
 }
